20a1ea56495dc4fefec754754181b2428930217f
Amanyti <160357843+Amanyti@users.noreply.github.com>
2024-03-18 22:20:51 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,11 @@
+const searchInput = document.getElementById('search-input');
+const resultArtists = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+document.addEventListener('input', function(){
+    const searchTerm = searchInput.value.toLoweCase();
+    if(searchTerm === ''){
+        result-playlists.Classlist
+
+    }
+})
